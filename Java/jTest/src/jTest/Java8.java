6e0e53fbe17a40f50b50467791d7c4e7a6b5ab93
i2ssbr <Sabino.Rodrigues@i2s.pt>
2017-01-24 13:59:44 +0000

    Java examples - fix Java version & remove unnecessary dependencies

--- Java/jTest/src/jTest/Java8.java
+++ Java/jTest/src/jTest/Java8.java
@@ -7,20 +7,7 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 public class Java8 {
-
-	private static Log log;
-
-	public static Log getLog() {
-		if (log == null) {
-			log = LogFactory.getLog("teste");
-		}
-		return log;
-	}
-
 	public static class Testando {
 		public static boolean inferiorTo10(Integer val) {
 			return val < 10;
